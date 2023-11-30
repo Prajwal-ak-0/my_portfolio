96d6709213d3daf0269c8d3c378998d968112262
Prajwal <prajwalhassan7@gmail.com>
2023-11-30 08:44:08 +0530

    Skills Section Started

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,24 +3,30 @@
 
 const About = () => {
   return (
-    <div className="z-10 mx-10 bg-transparent text-white ">
+    <div className="z-10 mb-4 md:mx-16 mx-10 bg-transparent text-white ">
       <h1 className="text-[70px] font-bold">
         About
       </h1>
       <div className="bg-gradient-to-tr from-[#024296] to-[#021e42] p-4 rounded-md">
-        <h2 className=" text-[40px] ">
-          Introduction :
-        </h2>
-        <p className="text-justify md:text-lg">
-          Hello, I'm Prajwal A K, a second-year Computer Science Engineering student at MS Ramaiah Institute of Technology. My journey in the world of technology began with Data Structures and Algorithms in C++. Over the past 1.5 years, I've mastered DSA concepts and continually practice to sharpen my skills.
+        <div>
+          <h2 className=" text-[40px]  font-semibold">
+            Introduction :
+          </h2>
+          <p className="text-justify sm:tracking-wide md:text-lg">
+            Hello, I'm Prajwal A K, a second-year Computer Science Engineering student at MS Ramaiah Institute of Technology. My journey in the world of technology began with Data Structures and Algorithms in C++. Over the past 1.5 years, I've mastered DSA concepts and continually practice to sharpen my skills.
 
-          Moving forward, I ventured into web development, starting with HTML, CSS, and JavaScript. Building on my foundation, I transitioned to React and developed various projects. Expanding into full-stack development, I explored the MERN stack, incorporating technologies like Tailwind CSS and integrating OpenAI APIs.
+            Moving forward, I ventured into web development, starting with HTML, CSS, and JavaScript. Building on my foundation, I transitioned to React and developed various projects. Expanding into full-stack development, I explored the MERN stack, incorporating technologies like Tailwind CSS and integrating OpenAI APIs.
 
-          In the dynamic web landscape, I embraced the rise of Next.js, mastering its concepts and applying them to real-world projects. To add a creative touch, I delved into Three.js and related libraries, crafting immersive 3D experiences. My proficiency extends to utilizing APIs and cloud services.
+            In the dynamic web landscape, I embraced the rise of Next.js, mastering its concepts and applying them to real-world projects. To add a creative touch, I delved into Three.js and related libraries, crafting immersive 3D experiences. My proficiency extends to utilizing APIs and cloud services.
 
-          As I progress in my academic and coding journey, I'm excited about the endless possibilities technology offers. Join me on this exploration as we dive into innovation and create meaningful solutions.
-
-        </p>
+            As I progress in my academic and coding journey, I'm excited about the endless possibilities technology offers. Join me on this exploration as we dive into innovation and create meaningful solutions.
+          </p>
+        </div>
+        <div className="mt-2">
+          <h2 className=" text-[40px]  font-semibold">
+            Skills :
+          </h2>
+        </div>
       </div>
     </div>
   )
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                     <div className="text-white ml-8 sm:text-left text-center">
                         <div className="mb-2">
                             <h1 className="font-black max-sm:mt-[100px] md:text-[80px] sm:text-[60px] xs:text-[50px] text-[45px] sm:leading-[90px] leading-[60px] mt-2 mb-2">
-                                Hey 👋, I'm <span className='text-[#915EFF]'>Prajwal</span>
+                                Hey 👋, I'm <span className='text-[#024296d6]'>Prajwal</span>
                             </h1>
                         </div>
                         <div className="mb-4 max-sm:mt-8">
